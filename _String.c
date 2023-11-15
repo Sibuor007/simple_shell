162dda758e2dfaa5974b3cb13fe8e30ef46fd375
Geneovo <bodjuvwederhie@gmail.com>
2023-11-15 07:43:31 -0800

    simple shell's string manipulation functions

--- _String.c
+++ _String.c
@@ -1,13 +1,13 @@
 #include "_Shell.h"
 
 /**
- * _strlen - returns the length of a string
+ * str_len - returns the length of a string
  *
  * @str: string whose length to check
  *
  * Return: int length of the string
  */
-int _strlen(char *str)
+int str_len(char *str)
 {
 	int i = 0;
 
@@ -19,14 +19,14 @@ int _strlen(char *str)
 }
 
 /**
- * _strcmp - this compares two strings in a natural order
+ * str_cmp - this compares two strings in a natural order
  *
  * @s1: first string input
  * @s2: second string input
  *
  * Return: negative if s1 < s2, positive if s1 > s2, 0 if s1 == s2
  */
-int _strcmp(char *s1, char *s2)
+int str_cmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
 	{
@@ -42,14 +42,14 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if a string starts with a specified prefix
+ * _start - checks if a string starts with a specified prefix
  *
  * @str: string to search
  * @prefix: prefix to find
  *
  * Return: address of next character of the string after the prefix or NULL
  */
-char *starts_with(const char *str, const char *prefix)
+char *_start(const char *str, const char *prefix)
 {
 	while (*prefix)
 		if (*prefix++ != *str++)
@@ -58,14 +58,14 @@ char *starts_with(const char *str, const char *prefix)
 }
 
 /**
- * _strcat - this concatenates two strings
+ * str_cat - this concatenates two strings
  *
  * @dest: the destination buffer
  * @src: source buffer
  *
  * Return: pointer to destination buffer
  */
-char *_strcat(char *dest, char *src)
+char *str_cat(char *dest, char *src)
 {
 	char *s = dest;
 
