a2a4f64686b0a35342a38fb435fcca37b0bedd76
Geneovo <bodjuvwederhie@gmail.com>
2023-11-17 15:47:16 -0800

    add these

--- _Environ.c
+++ _Environ.c
@@ -85,7 +85,7 @@ int env_populate(_info_pass_t *info_)
 	size_t x;
 
 	for (x = 0; _env_rn[x]; x++)
-		node_add_end(&n_node, get_env_rn[x], 0);
+		node_add_end(&n_node, _env_rn[x], 0);
 	info_->_env_copy = n_node;
 	return (0);
 }
